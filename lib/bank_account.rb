2fb4e6ed9253a9caafee0ecae2505db08b97420a
njerimk <nmurugikamau@gmail.com>
2020-10-13 15:27:17 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class BankAccount
+
+    attr_accessor :balance, :status
+    attr_reader :name
+    
+    @@all = []
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+        # binding.pry
+    end
+
+    def name=()
+        @name = name
+    end
+
+    def deposit(deposit)
+      self.balance += deposit 
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+        
+    def self.all 
+        @@all 
+    end
+   
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,52 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status
+  attr_reader :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    self.sender != nil && self.receiver != nil
+    sender.valid?
+    receiver.valid?
+  end
+
+  def execute_transaction
+      # if self.status == "complete"
+      #   return
+      # #self.valid = true
+      # elsif self.valid? == false || self.sender.balance < self.amount
+      #   self.status = "rejected" 
+      #   "Transaction rejected. Please check your account balance."
+      # elsif self.valid?
+      #   self.receiver.balance += self.amount
+      #   self.sender.balance -= self.amount
+      #   self.status = "complete"
+      # else
+      # end
+    if self.valid? && self.status == "pending" && self.sender.balance >= self.amount
+      self.receiver.balance += self.amount
+      self.sender.balance -= self.amount
+      self.status = "complete"
+    else
+      self.status = "rejected" 
+      "Transaction rejected. Please check your account balance."
+    end
+
+      # binding.pry
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
+  end
+
 end
